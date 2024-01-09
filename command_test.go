b88a4d43ee408b6369168eeb1f4ade812c4dd73e
Thim Cederlund <thim@cederlund.de>
2024-01-09 17:57:12 +0100

    test: add tests for the delete command (d)

--- command_test.go
+++ command_test.go
@@ -67,7 +67,6 @@ func TestCmdAppendLines(t *testing.T) {
 	}
 }
 
-
 // TestCmdChangeLines tests the (c)hange command.
 func TestCmdChangeLines(t *testing.T) {
 	var ted *Editor = NewEditor(nil, io.Discard, io.Discard)
@@ -126,6 +125,60 @@ func TestCmdChangeLines(t *testing.T) {
 		})
 	}
 
+}
+
+// TestCmdDeleteLines tests the (d)elete command.
+func TestCmdDeleteLines(t *testing.T) {
+	var ted *Editor = NewEditor(nil, io.Discard, io.Discard)
+	log.SetOutput(io.Discard)
+
+	tests := []struct {
+		input          []byte
+		expectError    bool
+		buffer         []string
+		expectedBuffer []string
+	}{
+		{
+			input:          []byte("1,2d"),
+			expectError:    false,
+			buffer:         []string{"hello", "world", "!"},
+			expectedBuffer: []string{"!"},
+		},
+		{
+			input:          []byte("2d"),
+			expectError:    false,
+			buffer:         []string{"hello", "world", "!"},
+			expectedBuffer: []string{"hello", "!"},
+		},
+		{
+			input:          []byte("d"),
+			expectError:    true,
+			buffer:         []string{"hello", "world", "!"},
+			expectedBuffer: []string{"hello", "world"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(string(test.input), func(t *testing.T) {
+			ted.setupTestFile(test.buffer)
+			ted.ReadInput(bytes.NewBuffer(test.input))
+			if err := ted.DoRange(); err != nil && !test.expectError {
+				t.Fatalf("expected no error, got %s", err)
+			}
+			if err := ted.DoCommand(); err != nil && !test.expectError {
+				t.Fatalf("expected no error, got %s", err)
+			}
+			if len(test.expectedBuffer) != len(ted.Lines) {
+				t.Fatalf("expected the total line count to be %d, got %d",
+					len(test.expectedBuffer), len(ted.Lines))
+			}
+			for i := 0; i < len(ted.Lines); i++ {
+				if ted.Lines[i] != test.expectedBuffer[i] {
+					t.Errorf("expected line %d to be '%s', got '%s'",
+						i, test.expectedBuffer[i], ted.Lines[i])
+				}
+			}
+		})
+	}
 }
 
 // TestCmdJoinLines tests the (j)oin command.
